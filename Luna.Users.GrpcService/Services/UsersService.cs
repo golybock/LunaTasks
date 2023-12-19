497a4a49d67051f82bcc80e9946051fbb4b39ae8
golybock <teplovartem3094@gmail.com>
2023-12-19 18:06:02 +0300

    start write users service on grpc

--- Luna.Users.GrpcService/Services/UsersService.cs
+++ Luna.Users.GrpcService/Services/UsersService.cs
@@ -1,4 +1,7 @@
-﻿using Luna.Users.Repositories.Repositories;
+﻿using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Luna.Users.Repositories.Repositories;
 
 namespace Luna.Users.GrpcService.Services;
 
@@ -13,5 +16,43 @@ public class UsersService : GrpcService.UsersService.UsersServiceBase
 		_userRepository = userRepository;
 	}
 
+	public override async Task<UsersList> GetUsersAsync(GetUsersAsyncRequest request, ServerCallContext context)
+	{
+		var users = await _userRepository.GetUsers();
 
+		var grpcUsers = users.Select(ToGrpcUserDatabase);
+
+		var result = new RepeatedField<UserDatabase>();
+
+		result.AddRange(grpcUsers);
+
+		return new UsersList() {Users = {result}};
+	}
+
+	public override async Task<UserResponse> GetUserByIdAsync(GetUserAsyncRequest request, ServerCallContext context)
+	{
+		var id = Guid.Parse(request.Id);
+
+		var user = await _userRepository.GetUserAsync(id);
+
+		if (user == null)
+			return new UserResponse();
+
+		var result = new UserResponse() {User = ToGrpcUserDatabase(user)};
+
+		return result;
+	}
+
+	private UserDatabase ToGrpcUserDatabase(SharedModels.Database.Users.UserDatabase userDatabase)
+	{
+		return new UserDatabase()
+		{
+			Id = userDatabase.Id.ToString(),
+			Email = userDatabase.Email,
+			CreatedTimestamp = Timestamp.FromDateTime(userDatabase.CreatedTimestamp),
+			Username = userDatabase.Username,
+			EmailConfirmed = userDatabase.EmailConfirmed,
+			PhoneNumber = userDatabase.PhoneNumber
+		};
+	}
 }
