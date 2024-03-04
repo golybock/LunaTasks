3997617c1c1cd4cb1c8ec896f500a52ccd2d4800
golybock <teplovartem3094@gmail.com>
2024-03-04 23:34:58 +0300

    created icons and header

---
+++ luna/src/components/signIn/SignIn.tsx
@@ -0,0 +1,36 @@
+﻿import React from "react";
+import './SignIn.css';
+
+interface IProps {
+
+}
+
+interface IState {
+
+}
+
+export default class SignIn extends React.Component<IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                {/*header*/}
+                <div className="App-Header">
+                    <div className="App-Logo-Block">
+                        <img className="App-Icon" src="/resources/icon.png" alt=""/>
+                        <label>Luna</label>
+                    </div>
+                    <div className="App-Header-Language">
+                        <label>Language</label>
+                    </div>
+                </div>
+                {/*<h1>Sign In</h1>*/}
+            </div>
+        );
+    }
+
+}
