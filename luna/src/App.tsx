d3772a0f17422b226248702b339f2676e55758af
golybock <teplovartem3094@gmail.com>
2024-03-05 12:18:24 +0300

    added background

--- luna/src/App.tsx
+++ luna/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SignIn from "./components/signIn/SignIn";
 
@@ -8,18 +7,7 @@ function App() {
         <div className="container">
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+                    <img src="/resources/background-transparent.png" className="App-logo" alt="logo"/>
                 </header>
             </div>
 
