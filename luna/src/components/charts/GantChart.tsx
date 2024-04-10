7d3be9c1dcf8aacb53cb7cf3f16b8d4849420369
golybock <teplovartem3094@gmail.com>
2024-04-10 20:22:02 +0300

    dont work gant

--- luna/src/components/charts/GantChart.tsx
+++ luna/src/components/charts/GantChart.tsx
@@ -1,27 +1,16 @@
 ﻿import React from "react";
-import {Gantt, Task} from "gantt-task-react";
+import {Gantt, Task, ViewMode} from 'gantt-task-react';
+import "gantt-task-react/dist/index.css";
 import ICardView from "../../models/card/view/cardView";
+import "./GantChart.css";
 
 interface IProps{
     cards: ICardView[]
 }
 
 interface IState{
-
+    tasks: Task[]
 }
-
-let tasks: Task[] = [
-    {
-        start: new Date(2020, 1, 1),
-        end: new Date(2020, 1, 2),
-        name: 'Idea',
-        id: 'Task 0',
-        type:'task',
-        progress: 45,
-        isDisabled: true,
-        styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
-    }
-];
 
 export default class GantChart extends React.Component<IProps, IState>{
 
@@ -29,19 +18,36 @@ export default class GantChart extends React.Component<IProps, IState>{
         super(props);
 
         this.state = {
-
+            tasks: []
         }
     }
 
     componentDidMount() {
-        // create tasks to view
+        const arr: Task[] = []
+
+        this.props.cards.forEach(item => {
+            arr.push({
+                start: new Date(2024, 1, 1),
+                end: new Date(2024, 1, 2),
+                name: item.header,
+                id: item.id,
+                type:'task',
+                progress: 100,
+                isDisabled: true,
+                styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
+            })
+        })
+
+        this.setState({tasks: arr})
     }
 
     render() {
         return (
-            <div>
-                <label>Пока ее нет</label>
-                {/*<Gantt tasks={tasks}/>*/}
+            <div className="Diagram">
+                {/*<Gantt tasks={this.state.tasks}*/}
+                {/*       columnWidth={40}*/}
+                {/*       listCellWidth=""*/}
+                {/*       locale={"ru"}/>*/}
             </div>
         );
     }
